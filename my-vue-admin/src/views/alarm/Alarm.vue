5092ad272da794050d287d63be148dffe45467eb
ShuaiFu <1729166956@qq.com>
2025-04-06 23:23:36 +0800

    分步表单二次封装

--- my-vue-admin/src/views/alarm/Alarm.vue
+++ my-vue-admin/src/views/alarm/Alarm.vue
@@ -20,17 +20,92 @@
                 <span v-else>{{ val }}</span>
             </el-descriptions-item>
             <el-descriptions-item label="操作">
-                <el-button :type="item.status === 2 ? 'warning' : 'primary'">{{ item.status === 1 ? "指派" : (item.status
-                    ===
-                    2 ? "催办" : "查看") }}</el-button>
+                <el-button @click="drawer = true" :type="item.status === 2 ? 'warning' : 'primary'">{{ item.status === 1
+                    ?
+                    "指派" : (item.status
+                        ===
+                        2 ? "催办" : "查看") }}</el-button>
             </el-descriptions-item>
         </el-descriptions>
     </el-card>
+    <el-drawer v-model="drawer" title='报警任务指派'>
+        <StepForm :steps="steps" :form1="form1" :form2="form2" :form3="form3" @handle-submit="handleSubmit">
+            <template #step-1>
+                <el-form :model="formData.basicInfo" :rules="basicRules" ref="form1">
+                    <el-form-item label="姓名" prop="name">
+                        <el-input v-model="formData.basicInfo.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱" prop="email">
+                        <el-input v-model="formData.basicInfo.email"></el-input>
+                    </el-form-item>
+                    <el-form-item label="电话" prop="tel">
+                        <el-input v-model="formData.basicInfo.tel"></el-input>
+                    </el-form-item>
+                    <el-form-item label="工号" prop="no">
+                        <el-input v-model="formData.basicInfo.no"></el-input>
+                    </el-form-item>
+                    <el-form-item label="是否加急">
+                        <el-switch v-model="formData.basicInfo.urgent"></el-switch>
+                    </el-form-item>
+                    <el-form-item label="其他选项">
+                        <el-checkbox-group v-model="formData.basicInfo.other">
+                            <el-checkbox value="1">更换设备</el-checkbox>
+                            <el-checkbox value="2">仅维修</el-checkbox>
+                            <el-checkbox value="3">需拍照片</el-checkbox>
+                            <el-checkbox value="4">需报备</el-checkbox>
+                        </el-checkbox-group>
+                    </el-form-item>
+                    <el-form-item label="其他备注信息">
+                        <el-input v-model="formData.basicInfo.remarks" type="textarea"></el-input>
+                    </el-form-item>
+                </el-form>
+            </template>
+            <template #step-2>
+                <el-form :model="formData.checkInfo" ref="form2" :rules="checkInfoRules">
+                    <el-form-item label="审批部门" prop="dep1">
+                        <el-select placeholder="请选择审批部门" v-model="formData.checkInfo.dep1">
+                            <el-option label="总裁办" value="1"></el-option>
+                            <el-option label="运营部" value="2"></el-option>
+                            <el-option label="维修部" value="3"></el-option>
+                            <el-option label="市场部" value="4"></el-option>
+                            <el-option label="财务部" value="5"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="抄送部门" prop="dep2">
+                        <el-select placeholder="请选择抄送部门" v-model="formData.checkInfo.dep2">
+                            <el-option label="总裁办" value="1"></el-option>
+                            <el-option label="运营部" value="2"></el-option>
+                            <el-option label="维修部" value="3"></el-option>
+                            <el-option label="市场部" value="4"></el-option>
+                            <el-option label="财务部" value="5"></el-option>
+                        </el-select>
+                    </el-form-item>
+                </el-form>
+            </template>
+            <template #step-3>
+                <el-form :model="formData.principalInfo" ref="form3" :rules="infoRules">
+                    <el-form-item label="负责人姓名" prop="person">
+                        <el-input v-model="formData.principalInfo.person"></el-input>
+                    </el-form-item>
+                    <el-form-item label="负责人电话" prop="tel">
+                        <el-input v-model="formData.principalInfo.tel"></el-input>
+                    </el-form-item>
+                </el-form>
+            </template>
+        </StepForm>
+        <el-result icon="warning" title="设备编号：CD1001" sub-title="该任务已催促2次，请抓紧处理">
+            <template #extra>
+                <el-button type="primary" @click="drawer = false">我已知晓</el-button>
+            </template>
+        </el-result>
+    </el-drawer>
 </template>
 <script setup lang="ts">
 import { onMounted, ref } from "vue"
 import { alarmListApi } from "@/api/alarm"
 import { getLabel } from "./fieldLabelMap"
+import StepForm from "@/components/stepForm/StepForm.vue"
+import { ElMessage, FormInstance } from "element-plus"
 interface AlarmListType {
     description: string,
     address: string,
@@ -46,4 +121,71 @@ onMounted(async () => {
     const { data } = await alarmListApi()
     alarmList.value = data
 })
+const drawer = ref<boolean>(false)
+const steps = [
+    { title: "基本信息" },
+    { title: "审批信息" },
+    { title: "负责人信息" }
+]
+const formData = ref({
+    basicInfo: {
+        name: "",
+        email: "",
+        tel: "",
+        no: "",
+        urgent: true,
+        other: [],
+        remarks: ''
+    },
+    checkInfo: {
+        dep1: '',
+        dep2: ''
+    },
+    principalInfo: {
+        person: '',
+        tel: ''
+    }
+})
+const form1 = ref<FormInstance>()
+const basicRules = {
+    name: [
+        { required: true, message: "请输入姓名", trigger: "blur" }
+    ],
+    email: [
+        { required: true, message: "请输入邮箱", trigger: "blur" }
+    ],
+    tel: [
+        { required: true, message: "请输入电话", trigger: "blur" }
+    ],
+    no: [
+        { required: true, message: "请输入工号", trigger: "blur" }
+    ]
+}
+const form2 = ref<FormInstance>()
+const checkInfoRules = {
+    dep1: [
+        { required: true, message: "不能为空", trigger: "blur" }
+    ],
+    dep2: [
+        { required: true, message: "不能为空", trigger: "blur" }
+    ]
+}
+const form3 = ref<FormInstance>()
+const infoRules = {
+    person: [
+        { required: true, message: "不能为空", trigger: "blur" }
+    ],
+    tel: [
+        { required: true, message: "不能为空", trigger: "blur" }
+    ],
+}
+const handleSubmit = () => {
+    console.log(formData.value)
+    //把formData提交给后端
+    ElMessage({
+        message: '提交成功',
+        type: "success"
+    })
+    drawer.value = false
+}
 </script>
